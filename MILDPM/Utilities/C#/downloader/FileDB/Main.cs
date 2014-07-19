17d3b7e14aa34e23b8337dbbd79d873132fa4693
Mycroft <joshmilesproduction@gmail.com>
2014-07-19 22:35:49 +0100

    Updated downloader and created file DB

---
+++ MILDPM/Utilities/C#/downloader/FileDB/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace FileDB
+{
+	class MainClass
+	{
+		private static TcpListener listener;
+        private static int LIMIT = 50;
+        private static int port = 8000;
+		public static void Main (string[] args)
+		{
+			IPAddress ip;
+            ip = Dns.GetHostEntry("localhost").AddressList[1];
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine("FileDB");
+            Console.WriteLine("Server mounted, listening on " + ip + ":" + port);
+
+            for (int i = 0; i < LIMIT; i++)
+            {
+                Thread t = new Thread(new ThreadStart(Service));
+                t.Start();
+            }
+		}
+		       public static void Service()
+        {
+            string COL_DOM = "";
+            while (true)
+            {
+                Socket soc = listener.AcceptSocket();
+                try
+                {
+                    bool ip = false;
+                    Console.WriteLine("Opened connection.");
+                    Stream s = new NetworkStream(soc);
+                    StreamReader sr = new StreamReader(s);
+                    StreamWriter sw = new StreamWriter(s);
+                    sw.AutoFlush = true;
+					string uniquePackageIdentifier = sr.ReadLine ();
+					sw.WriteLine ("Recieved");
+					Console.WriteLine ("Client requested " + uniquePackageIdentifier);										
+                    //to be continued
+					soc.Close();
+                    Console.WriteLine("Closed connection");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("ERROR: " + e.ToString());
+                    Console.WriteLine("PARAMETERS: " + COL_DOM);
+                }
+                finally
+                {
+                    soc.Close();
+                }
+            }
+	}
+}
--- MILDPM/Utilities/C#/downloader/downloader/Main.cs
+++ MILDPM/Utilities/C#/downloader/downloader/Main.cs
@@ -1,15 +1,35 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace downloader
 {
 	class MainClass
 	{
+		public string ServerIP = "127.0.0.1";
+		public int ServerPort = 8000;
 		public static void Main (string[] args)
 		{
-			if(args[] == 0){
-				Console.WriteLine("Invalid arguments.");	
+			if(args.Length == 0){
+				Console.WriteLine ("Invalid arguments.");	
 			}else{
-				
+				string uniquePackageIdentifier = args[1];
+                TcpClient tcp = new TcpClient(ServerIP, ServerPort);
+                Stream z = tcp.GetStream();
+                StreamReader zr = new StreamReader(z);
+                StreamWriter zw = new StreamWriter(z);
+                zw.AutoFlush = true;
+				zw.WriteLine (uniquePackageIdentifier);
+				string wait = zr.ReadLine (); // waiting for a response from the file db
+				//to be continued
+				zw.Close ();
+				zr.Close ();
+				tcp.Close ();
 			}
 		}
 	}
